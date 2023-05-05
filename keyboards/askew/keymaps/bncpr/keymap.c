6700c1f95bd9299b334cc2b53c0b6ba09d6bf6e3
Ben Cooper <mnib.mnib@gmail.com>
2023-05-05 17:40:23 +0000

    ingore mod tap interrupt breaking change

--- keyboards/askew/keymaps/bncpr/keymap.c
+++ keyboards/askew/keymaps/bncpr/keymap.c
@@ -208,16 +208,19 @@ void matrix_scan_user(void) {
     }
 }
 
-bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t* record) {
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case SFT_T(KC_BSPC):
-        case CTL_T(KC_SPC):
-            // Do not force the mod-tap key press to be handled as a modifier
-            // if any other key was pressed while the mod-tap key is held down.
-            return true;
+        // Capture all mod-tap keycodes.
+        case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+            if (keycode == SFT_T(KC_BSPC) || keycode == CTL_T(KC_SPC)) {
+                // Disable HOLD_ON_OTHER_KEY_PRESS for LCTL_T(KC_A)
+                // aka enable IGNORE_MOD_TAP_INTERRUPT for LCTL_T(KC_A).
+                return false;
+            } else {
+                // Enable HOLD_ON_OTHER_KEY_PRESS for every other mod-tap keycode.
+                return true;
+            }
         default:
-            // Force the mod-tap key press to be handled as a modifier if any
-            // other key was pressed while the mod-tap key is held down.
             return false;
     }
 }
